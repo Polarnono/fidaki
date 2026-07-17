b2014166b98166835de6d5d3285a1234d91a72bc
Polarnono <skourisgeorge0@gmail.com>
2026-07-17 03:26:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fidaki
+# fidaki
---
+++ snake&ladders.py
@@ -0,0 +1,79 @@
+import pygame
+import sys
+
+
+pygame.init()
+
+#screen size
+size_x = 800
+size_y = 800
+screen = pygame.display.set_mode((size_x,size_y))
+
+tile_size = 80
+
+#colors
+BG = (225,225,225)
+YELLOW = (255,255,0)
+RED = (255,0,0)
+BLACK=(0,0,0)
+
+def draw_num(tile_size):
+    font = pygame.font.Font(None,36)
+
+#Making the numbers go in a Zig-Zag Pattern. Explanation: Splitting the rows in even and odds and then applying the equation (removing from the 100 due to 0 being the top left corner): (100 - [the number of the row]*10).
+#Then based on the direction we want to go we remove simply the col_indx if we want left to right or reverse it by doing 9-col_indx. 
+
+    for y in range (0, size_y, tile_size):
+        row_indx=(y//80)
+        if (y//80) % 2 == 0:
+            for x in range(0, size_x, tile_size):
+                col_indx=(x//80)
+                number=100-(row_indx*10)-col_indx
+                txt_img= font.render(str(number),True,BLACK)
+                text_rect= txt_img.get_rect()
+                text_rect.center = (x+40,y+40)
+                screen.blit(txt_img,text_rect)
+        else:
+            for x in range(0,size_x,tile_size):
+                col_indx=(x//80)
+                number=100-(row_indx*10)-(9-col_indx)
+                txt_img= font.render(str(number),True,BLACK)
+                text_rect= txt_img.get_rect()
+                text_rect.center = (x+40,y+40)
+                screen.blit(txt_img,text_rect)
+
+
+def draw_grid(tile_size):
+    screen.fill(BG)
+
+    #draws vertical lines
+    for x in range(tile_size, size_x, tile_size):
+        pygame.draw.line(screen,YELLOW,(x,0),(x,size_y))
+
+    #draw horizontal lines
+    for y in range(tile_size, size_y, tile_size):
+        pygame.draw.line(screen,YELLOW, (0,y), (size_x,y))
+
+
+
+run = True ;
+
+#MAIN LOOP
+while ( run == True):
+   
+    draw_grid(tile_size)
+
+    draw_num(tile_size)
+    
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+
+
+    pygame.display.update()
+    
