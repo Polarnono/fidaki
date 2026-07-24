d165b6cb93093265cb4a2bf7e93b66c1e9cc90e2
Polarnono <skourisgeorge0@gmail.com>
2026-07-24 03:54:40 +0300

    added color on odd numbers and find_pos method

--- snake&ladders.py
+++ snake&ladders.py
@@ -17,6 +17,24 @@ YELLOW = (255,255,0)
 RED = (255,0,0)
 BLACK=(0,0,0)
 
+#dictionaries
+snakes = {97:78,95:56,88:24,63:18,36:6,48:26,32:10}
+ladder = {1:38,4:14,8:30,21:42,28:76,71:92,86:99}
+
+#Draws red background every odd number (called before the number is drawn on the grid)
+def draw_bg_grid(x,y):
+    pygame.draw.rect(screen,RED,(x,y,tile_size,tile_size))
+
+def find_pos(tile_number):
+    row=(100-tile_number)//10
+    col=(tile_number-1)%10
+    y=row*80+40
+    if (row%2 == 0):
+        x =(9-col)*80+40
+    else:
+        x=col*80+40
+    return(x,y)
+
 def draw_num(tile_size):
     font = pygame.font.Font(None,36)
 
@@ -29,6 +47,8 @@ def draw_num(tile_size):
             for x in range(0, size_x, tile_size):
                 col_indx=(x//80)
                 number=100-(row_indx*10)-col_indx
+                if (number%2)!=0: 
+                    draw_bg_grid(x,y)
                 txt_img= font.render(str(number),True,BLACK)
                 text_rect= txt_img.get_rect()
                 text_rect.center = (x+40,y+40)
@@ -37,6 +57,8 @@ def draw_num(tile_size):
             for x in range(0,size_x,tile_size):
                 col_indx=(x//80)
                 number=100-(row_indx*10)-(9-col_indx)
+                if (number%2)!=0: 
+                    draw_bg_grid(x,y)
                 txt_img= font.render(str(number),True,BLACK)
                 text_rect= txt_img.get_rect()
                 text_rect.center = (x+40,y+40)
@@ -60,7 +82,9 @@ run = True ;
 
 #MAIN LOOP
 while ( run == True):
-   
+
+    print("p[os]",find_pos(42))
+
     draw_grid(tile_size)
 
     draw_num(tile_size)
